9d35cfed560c6bac1e3ce2d6f57d8d582da9fbf2
Lily <8723107+lily-gh@users.noreply.github.com>
2024-12-27 18:33:59 +0100

    Added instruction to install xcode command line tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,17 @@
 
 # todo: make it work for both macos and ubuntu/arch
 
-# Get the user base path
-PYTHON_BASEPATH=$(python3 -m site --user-base)
+# Install xcode command line tools
+xcode-select --install
+sudo xcodebuild -license
 
 # Install ansible using pip in user mode
 python3 -m pip install --user ansible
 
-# Add the user base bin directory to PATH
+# Get the python base path
+PYTHON_BASEPATH=$(python3 -m site --user-base)
+
+# Add the base bin directory of preinstalled python to PATH
 export PATH="$PATH:$PYTHON_BASEPATH/bin"
 
 ansible-playbook main.yml --ask-become-pass
