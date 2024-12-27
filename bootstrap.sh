c07cf061cf7a5ea2c3b074917328e1c0933b3e8b
Lily <8723107+lily-gh@users.noreply.github.com>
2024-12-27 18:58:11 +0100

    Minor fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,6 @@
 
 # todo: make it work for both macos and ubuntu/arch
 
-# Install xcode command line tools
-xcode-select --install
-sudo xcodebuild -license
 
 # Install ansible using pip in user mode
 python3 -m pip install --user ansible --ignore-installed
