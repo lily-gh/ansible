9315f7085c4cd4b0e40c02b9034fc812286ec957
Lily <8723107+lily-gh@users.noreply.github.com>
2024-12-31 00:08:18 +0100

    Minor fix

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 
 USER_BASE=$(python3 -m site --user-base)
-ANSIBLE_PATH=$(find "$USER_BASE/lib/" -type d -path "*/site-packages/ansible" 2>/dev/null)
+ANSIBLE_PATH=$(find "$USER_BASE/lib" -type d -path "*/site-packages/ansible" 2>/dev/null)
 
 if [ -n "$ANSIBLE_PATH" ]; then
   echo "Ansible is already installed at: $ANSIBLE_PATH"
