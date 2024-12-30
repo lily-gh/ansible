7399a8cfc21471db1ab3d6744ad5f4586fdc7dc0
Lily <8723107+lily-gh@users.noreply.github.com>
2024-12-31 00:13:51 +0100

    Minor fix

--- test.sh
+++ test.sh
@@ -4,10 +4,10 @@
 USER_BASE=$(python3 -m site --user-base)
 ANSIBLE_PATH=$(find "$USER_BASE/lib" -type d -path "*/site-packages/ansible" 2>/dev/null)
 
-if [ -n "$ANSIBLE_PATH" ]; then
-  echo "Ansible is already installed at: $ANSIBLE_PATH"
+if ! command -v ansible &>/dev/null; then
+    echo "Installing Ansible with apt..."
 else
-  echo "Installing Ansible using pip..."
+    echo "Ansible is already installed."
 fi
 
 #ansible-playbook test.yml --ask-become-pass
